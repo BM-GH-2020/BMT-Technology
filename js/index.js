08ee8010620b35c87f34d39107bde26ee6550d90
Tino Christ <gc@192-168-1-103.tpgi.com.au>
2021-02-18 12:39:07 +1100

    Add datasets and functionality to done button

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 const taskManager = new TaskManager(0);
-  
 
 let form = document.querySelector("form");
 let userName = document.querySelector("#formGroupExampleInput");
@@ -10,107 +9,121 @@ let dateField = document.querySelector("#date");
 let submit = document.querySelector("#button");
 let invalidation = document.querySelector(".invalid-feedback");
 let validation = document.querySelector(".valid-feedback");
+let status = document.querySelector("#new-task-status");
 
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-
-
-form.addEventListener('submit', event => {
-    event.preventDefault();
-
-   /* taskManager.addTask(
+  /* taskManager.addTask(
         userName.value,
         about.value,
         roles.value,
         assign.value,
         dateField.value
     ) */
-    
-    
-    
-    const clearFormFields = () => {
-      userName.value = "";
-      about.value = "";
-      roles.value = "Select role";
-      assign.value = "Assign to";
-      dateField.value = "dd-mm-yyyy";
-      userName.classList.remove("is-valid");
-      about.classList.remove("is-valid");
-      roles.classList.remove("is-valid");
-      assign.classList.remove("is-valid");
-      dateField.classList.remove("is-valid");
 
-    };
+  const clearFormFields = () => {
+    userName.value = "";
+    about.value = "";
+    roles.value = "Select role";
+    assign.value = "Assign to";
+    dateField.value = "dd-mm-yyyy";
+    userName.classList.remove("is-valid");
+    about.classList.remove("is-valid");
+    roles.classList.remove("is-valid");
+    assign.classList.remove("is-valid");
+    dateField.classList.remove("is-valid");
+    status.classList.remove("is-valid");
+  };
 
+  let validationFailure = 0;
 
-    let validationFailure = 0;
+  if (userName.value.length < 5) {
+    console.log(userName.value);
+    userName.classList.add("is-invalid");
+    userName.classList.remove("is-valid");
+    validationFailure++;
+  } else if (userName.value.length > 5) {
+    userName.classList.remove("is-invalid");
+    userName.classList.add("is-valid");
+  }
 
-    if (userName.value.length < 5) {
-      console.log(userName.value);
-      userName.classList.add("is-invalid"); 
-      userName.classList.remove("is-valid"); 
-      validationFailure++;
-    } else if (userName.value.length > 5) {
-      userName.classList.remove("is-invalid"); 
-      userName.classList.add("is-valid");
-    }
-
-    if(about.value.length < 4) {
-        about.classList.add("is-invalid"); 
-      about.classList.remove("is-valid");  
-      validationFailure++;
-    } else if (userName.value.length > 4) {
-        userName.classList.remove("is-invalid"); 
-        userName.classList.add("is-valid");
-      }
-   /* if (dateField === NaN) {
+  if (about.value.length < 4) {
+    about.classList.add("is-invalid");
+    about.classList.remove("is-valid");
+    validationFailure++;
+  } else if (userName.value.length > 4) {
+    userName.classList.remove("is-invalid");
+    userName.classList.add("is-valid");
+  }
+  /* if (dateField === NaN) {
         dateField.classList.add("is-invalid"); 
       dateField.classList.remove("is-valid"); 
     } */
-     if (assign.value) {
-        assign.classList.remove("is-invalid"); 
-      assign.classList.add("is-valid");  
-    } else if (!assign.value) {
-        assign.classList.add("is-invalid"); 
-      assign.classList.remove("is-valid");
-      validationFailure++;
-    }
+  if (assign.value) {
+    assign.classList.remove("is-invalid");
+    assign.classList.add("is-valid");
+  } else if (!assign.value) {
+    assign.classList.add("is-invalid");
+    assign.classList.remove("is-valid");
+    validationFailure++;
+  }
 
-    if (roles.value) {
-        roles.classList.remove("is-invalid"); 
-      roles.classList.add("is-valid");  
-    } else if (!roles.value) {
-        roles.classList.add("is-invalid"); 
-      roles.classList.remove("is-valid");
-      validationFailure++;
-    }
+  if (roles.value) {
+    roles.classList.remove("is-invalid");
+    roles.classList.add("is-valid");
+  } else if (!roles.value) {
+    roles.classList.add("is-invalid");
+    roles.classList.remove("is-valid");
+    validationFailure++;
+  }
 
+  if (status.value) {
+    status.classList.add("is-valid");
+    status.classList.remove("is-invalid");
+  } else {
+    status.classList.add("is-invalid");
+    status.classList.remove("is-valid");
+    validationFail++;
+  }
 
   // If the validation fails, the function will not work
 
-    if (validationFailure > 0) {
-      validationFailure = 0;
-      return;
-    } else {
-      console.log(userName.value)
-      taskManager.addTask(
-       
-        userName.value,
-        about.value,
-        roles.value,
-        assign.value,
-        dateField.value
-      );
-      
-      clearFormFields();
-      taskManager.render();
-    }
+  if (validationFailure > 0) {
+    validationFailure = 0;
+    return;
+  } else {
+    console.log(status.value);
+    taskManager.addTask(
+      userName.value,
+      about.value,
+      roles.value,
+      assign.value,
+      dateField.value,
+      status.value
+    );
+
+    clearFormFields();
+    taskManager.render();
+  }
 });
 
+document.getElementById("tasks-list").innerHTML = taskManager.render();
 
+let taskList = document.querySelector("#tasks-list");
 
+taskList.addEventListener("click", (event) => {
+  if (event.target.classList.contains("done-button")) {
+    const parentTask =
+      event.target.parentElement.parentElement.parentElement.parentElement;
 
-
-
+    let taskId = Number(parentTask.dataset.taskId);
+    let task = taskManager.getTaskById(taskId);
+    task.status = "DONE";
+    taskManager.render();
+    console.log(task.status.value);
+  }
+});
 
 // const newtask = new taskManager('Apply for this job');
 // console.log(newtask.tasks);
--- js/taskManager.js
+++ js/taskManager.js
@@ -1,31 +1,42 @@
 //Code for displaying the task card html.
-const createTaskHtml = (userName, about, roles, assign, dateField) => {
+const createTaskHtml = (
+  id,
+  userName,
+  about,
+  roles,
+  assign,
+  dateField,
+  status
+) => {
   console.log(userName);
   const html = `
-        <li class="card" style="min-width: 50vw">
-        <div class="card-body">
-        <h5 class="card-title">${userName}</h5>
-        <p class="card-text">
-        ${about}
-        </p>
-        <p class="card-text">${roles} To</p>
-        <p class="card-text">${assign}</p>
-        <div class="card-footer row">
-        <div class="col-6">
-        <p class="card-text"><b>${dateField}</b></p>
-        </div>
-        <div class="col-3">
-        <button class="btn btn-outline-success done-button">
-        Done
-        </button>
-        </div>
-        <div class="col-3">
-        <button class="btn btn-outline-danger delete-button">
-        Delete
-        </button>
-        </div>
-        </div>
-        </div>
+        <li class="card" style="min-width: 25vw" data-task-id="${id}">
+            <div class="card-body">
+                <h5 class="card-title">${userName}</h5>
+                <p class="card-text">
+                ${about}
+                </p>
+                <p class="card-text">${roles} To</p>
+                <p class="card-text">${assign}</p>
+                <div class="card-footer row">
+                    <div class="col-6">
+                        <p class="card-text"><b>${dateField}</b></p>
+                    </div>
+                    <div class="col-6">
+                        <p class="card-text"><b>${status}</b></p>
+                    </div>
+                    <div class="col-3">
+                        <button class="btn btn-outline-success done-button">
+                        Done
+                        </button>   
+                    </div>
+                    <div class="col-3">
+                        <button class="btn btn-outline-danger delete-button">
+                        Delete
+                        </button>
+                    </div>
+                </div>
+            </div>
         </li>
         `;
   return html;
@@ -37,37 +48,53 @@ class TaskManager {
     this.currentId = currentId;
   }
 
-  addTask(userName, about, roles, assign, dateField) {
+  addTask(userName, about, roles, assign, dateField, status) {
     console.log(userName);
     this.currentId++;
     const task = {
       id: this.currentId++,
-
       userName: userName,
       about: about,
       assign: assign,
       roles: roles,
       dateField: dateField,
+      status: status,
     };
     this.tasks.push(task);
-    console.log(this.tasks);
+    //console.log(this.tasks);
+  }
+
+  getTaskById(taskId) {
+    let foundTask;
+    this.tasks.forEach((taskItem) => {
+      let task = taskItem;
+
+      if (task.id === taskId) {
+        foundTask = task;
+      }
+      console.log(foundTask);
+    });
+    return foundTask;
   }
 
   render() {
     const tasksHtmlList = [];
 
     this.tasks.forEach((task) => {
-      console.log(task);
+      //console.log(task);
       let date = new Date(task.dateField);
       let formattedDate = `Submitted on: ${
         (date.getDay(), date.getMonth(), date.getYear())
       }`;
       let taskHTML = createTaskHtml(
+        task.id,
+        console.log(task.id),
         task.userName,
         task.about,
         task.assign,
         task.roles,
-        formattedDate
+        formattedDate,
+        task.status
       );
       tasksHtmlList.push(taskHTML);
     });
